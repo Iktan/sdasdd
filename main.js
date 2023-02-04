3fde7c1178eada875806586a7432516abcc8d7c3
Iktan <94088670+Iktan@users.noreply.github.com>
2023-02-04 17:26:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+https//teachablemachine.withgoogle.com/models/y8n5GiNdR/
+WebKitCSSMatrix.set({
+    width:350
+    height:300
+    image_format : 'png' , 
+    png_quality:90
+})
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+console.log('ml5 version', ml5.version);
+classifier =ml5.imageClassifier('https//teachablemachine.withgoogle.com/models/y8n5GiNdR/')
